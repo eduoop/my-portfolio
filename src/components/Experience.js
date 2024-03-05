23409f9ae4317b1e6664fa34324326db90bb395d
Eduardo <eduardo.moraes@clicksoft.com.br>
2024-03-05 15:12:01 -0300

    feat: update portfolio

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -16,14 +16,14 @@ class Experience extends Component {
 
         var mainTech = mainTechnologies.map((technology, i) => {
           return (
-            <Badge pill className="main-badge mr-2 mb-2" key={i}>
+            <Badge style={{ fontFamily: "sans-serif" }} pill className="main-badge mr-2 mb-2" key={i}>
               {technology}
             </Badge>
           );
         });
         var tech = technologies.map((technology, i) => {
           return (
-            <Badge pill className="experience-badge mr-2 mb-2" key={i}>
+            <Badge style={{ fontFamily: "sans-serif" }} pill className="experience-badge mr-2 mb-2" key={i}>
               {technology}
             </Badge>
           );
